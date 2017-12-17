84fddff695f381bf9318cf5710abb105399f83c3
rajdeep1008 <iamrajdeep1008@gmail.com>
2017-12-17 19:20:52 +0530

    Initial commit

---
+++ app/src/main/java/io/github/rajdeep1008/cryptofolio/Constants.java
@@ -0,0 +1,9 @@
+package io.github.rajdeep1008.cryptofolio;
+
+/**
+ * Created by rajdeep1008 on 17/12/17.
+ */
+
+public class Constants {
+    public static String BASE_API = "https://api.coinmarketcap.com/v1/ticker/";
+}
