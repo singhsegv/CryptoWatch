790ace4f86e3ba28b7f56a2d1777961b324d5f7e
rajdeep1008 <iamrajdeep1008@gmail.com>
2018-01-11 02:50:32 +0530

    shifting to fragments (Warning: Sleepy while coding)

--- app/src/main/java/io/github/rajdeep1008/cryptofolio/activity/MainActivity.java
+++ app/src/main/java/io/github/rajdeep1008/cryptofolio/activity/MainActivity.java
@@ -5,41 +5,23 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ProgressBar;
-import android.widget.Toast;
-
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.github.rajdeep1008.cryptofolio.R;
-import io.github.rajdeep1008.cryptofolio.adapter.CryptoAdapter;
 import io.github.rajdeep1008.cryptofolio.extras.BottomNavigationViewHelper;
-import io.github.rajdeep1008.cryptofolio.model.Crypto;
-import io.github.rajdeep1008.cryptofolio.rest.ResponseCallback;
-import io.github.rajdeep1008.cryptofolio.rest.ServiceGenerator;
+import io.github.rajdeep1008.cryptofolio.fragment.FeedFragment;
 
 public class MainActivity extends AppCompatActivity {
-
-    @BindView(R.id.main_rv)
-    RecyclerView mainRv;
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
 
-    @BindView(R.id.prograss_bar)
-    ProgressBar progressBar;
-
     @BindView(R.id.bottom_navigation)
     BottomNavigationView bottomNav;
 
-    ServiceGenerator generator;
-    CryptoAdapter mAdapter;
     Context mContext;
 
     @Override
@@ -53,7 +35,6 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationViewHelper.disableShiftMode(bottomNav);
         bottomNav.setSelectedItemId(R.id.action_feed);
-        progressBar.setVisibility(View.VISIBLE);
 
         bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -64,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
                     case R.id.action_watchlist:
                         return true;
                     case R.id.action_feed:
+                        getSupportFragmentManager().beginTransaction().add(R.id.container, new FeedFragment()).commit();
                         return true;
                     case R.id.action_portfolio:
                         return true;
@@ -73,24 +55,5 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
-
-        generator = new ServiceGenerator();
-        generator.getFeed(new ResponseCallback<List<Crypto>>() {
-            @Override
-            public void success(List<Crypto> cryptos) {
-                progressBar.setVisibility(View.GONE);
-                List<Crypto> list = cryptos;
-                mAdapter = new CryptoAdapter(list, mContext);
-                mainRv.setLayoutManager(new LinearLayoutManager(mContext));
-                mainRv.setAdapter(mAdapter);
-            }
-
-            @Override
-            public void failure(List<Crypto> cryptos) {
-                progressBar.setVisibility(View.GONE);
-                Toast.makeText(mContext, "Error in loading", Toast.LENGTH_SHORT).show();
-            }
-        });
-
     }
 }
---
+++ app/src/main/java/io/github/rajdeep1008/cryptofolio/fragment/FeedFragment.java
@@ -0,0 +1,60 @@
+package io.github.rajdeep1008.cryptofolio.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.github.rajdeep1008.cryptofolio.R;
+import io.github.rajdeep1008.cryptofolio.adapter.CryptoAdapter;
+import io.github.rajdeep1008.cryptofolio.model.Crypto;
+import io.github.rajdeep1008.cryptofolio.rest.ResponseCallback;
+import io.github.rajdeep1008.cryptofolio.rest.ServiceGenerator;
+
+public class FeedFragment extends Fragment {
+
+    @BindView(R.id.main_rv)
+    RecyclerView mainRv;
+
+    @BindView(R.id.prograss_bar)
+    ProgressBar progressBar;
+
+    ServiceGenerator generator;
+    CryptoAdapter mAdapter;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_feed, container, false);
+        ButterKnife.bind(this, rootView);
+
+        progressBar.setVisibility(View.VISIBLE);
+        generator = new ServiceGenerator();
+        generator.getFeed(new ResponseCallback<List<Crypto>>() {
+            @Override
+            public void success(List<Crypto> cryptos) {
+                progressBar.setVisibility(View.GONE);
+                List<Crypto> list = cryptos;
+                mAdapter = new CryptoAdapter(list, getActivity());
+                mainRv.setLayoutManager(new LinearLayoutManager(getActivity()));
+                mainRv.setAdapter(mAdapter);
+            }
+
+            @Override
+            public void failure(List<Crypto> cryptos) {
+                progressBar.setVisibility(View.GONE);
+                Toast.makeText(getActivity(), "Error in loading", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return rootView;
+    }
+}
