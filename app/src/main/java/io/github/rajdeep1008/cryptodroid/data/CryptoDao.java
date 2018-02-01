3a663cd876e73cc89e16ff564b27b7d63e732feb
rajdeep1008 <iamrajdeep1008@gmail.com>
2018-02-01 14:30:31 +0530

    Portfolio section removed till 2nd release. Basic ui of detail activity working

---
+++ app/src/main/java/io/github/rajdeep1008/cryptodroid/data/CryptoDao.java
@@ -0,0 +1,40 @@
+package io.github.rajdeep1008.cryptodroid.data;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+/**
+ * Created by rajdeep1008 on 23/01/18.
+ */
+
+@Dao
+public interface CryptoDao {
+
+    @Insert
+    void insertAll(List<Crypto> cryptos);
+
+    @Update
+    void updateAll(List<Crypto> cryptos);
+
+    @Query("SELECT * FROM crypto")
+    List<Crypto> getAll();
+
+    @Query("SELECT * FROM crypto WHERE id IN (:ids)")
+    List<Crypto> loadFavorites(List<String> ids);
+
+    @Query("SELECT * FROM crypto WHERE id LIKE :cryptoId")
+    Crypto getById(String cryptoId);
+
+    @Query("SELECT * FROM crypto WHERE symbol LIKE :idQuery or name LIKE :nameQuery")
+    List<Crypto> searchCryptos(String idQuery, String nameQuery);
+
+    @Query("SELECT COUNT(*) from crypto")
+    int getCryptoCount();
+
+    @Query("DELETE FROM crypto")
+    void clearTable();
+}
---
+++ app/src/main/java/io/github/rajdeep1008/cryptodroid/fragment/FavoritesFragment.java
@@ -0,0 +1,151 @@
+package io.github.rajdeep1008.cryptodroid.fragment;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.preference.PreferenceManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.github.rajdeep1008.cryptodroid.R;
+import io.github.rajdeep1008.cryptodroid.adapter.CryptoAdapter;
+import io.github.rajdeep1008.cryptodroid.data.AppDatabase;
+import io.github.rajdeep1008.cryptodroid.data.Crypto;
+import io.github.rajdeep1008.cryptodroid.data.CryptoDao;
+import io.github.rajdeep1008.cryptodroid.extras.Utilities;
+import io.github.rajdeep1008.cryptodroid.rest.ResponseCallback;
+import io.github.rajdeep1008.cryptodroid.rest.ServiceGenerator;
+
+public class FavoritesFragment extends Fragment {
+
+    @BindView(R.id.main_rv)
+    RecyclerView mainRv;
+
+    @BindView(R.id.progress_bar)
+    ProgressBar progressBar;
+
+    @BindView(R.id.empty_tv)
+    TextView emptyTv;
+
+    @BindView(R.id.refresh_layout)
+    SwipeRefreshLayout refreshLayout;
+
+    private CryptoAdapter mAdapter;
+    private AppDatabase appDatabase;
+    private CryptoDao cryptoDao;
+    private ServiceGenerator generator;
+    private SharedPreferences prefs;
+    private SharedPreferences.OnSharedPreferenceChangeListener listener;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mAdapter = new CryptoAdapter(new ArrayList<Crypto>(), getActivity());
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_favorites, container, false);
+
+        ButterKnife.bind(this, rootView);
+        appDatabase = AppDatabase.getInstance(getActivity());
+        cryptoDao = appDatabase.cryptoDao();
+        generator = new ServiceGenerator();
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+            @Override
+            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+                if (s.equals("default_currency")) {
+                    loadFavorites(sharedPreferences.getString(s, "USD"));
+                }
+            }
+        };
+
+        prefs.registerOnSharedPreferenceChangeListener(listener);
+
+        mainRv.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mainRv.setAdapter(mAdapter);
+
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                loadFavorites(prefs.getString("default_currency", "USD"));
+                refreshLayout.setRefreshing(false);
+            }
+        });
+
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Log.d("test", "running");
+                loadFavorites(prefs.getString("default_currency", "USD"));
+                handler.postDelayed(this, 180000);
+            }
+        });
+    }
+
+    public void loadFavorites(String currency) {
+        final List<String> favoriteIds = Utilities.getFavorites(getActivity().getApplicationContext());
+
+        if (favoriteIds != null && favoriteIds.size() != 0) {
+            mainRv.setVisibility(View.VISIBLE);
+            emptyTv.setVisibility(View.GONE);
+
+            generator.getFeed(new ResponseCallback<List<Crypto>>() {
+                @Override
+                public void success(List<Crypto> cryptos) {
+                    emptyTv.setVisibility(View.GONE);
+                    progressBar.setVisibility(View.GONE);
+
+                    List<Crypto> favoriteList = new ArrayList<>();
+
+                    for (Crypto item : cryptos) {
+                        for (String id : favoriteIds) {
+                            if (item.getId().equals(id)) {
+                                favoriteList.add(item);
+                                break;
+                            }
+                        }
+                    }
+
+                    mAdapter.addAll(favoriteList, prefs.getString("default_currency", "USD"));
+                }
+
+                @Override
+                public void failure(List<Crypto> cryptos) {
+                    progressBar.setVisibility(View.GONE);
+                    Toast.makeText(getActivity(), "Error in loading", Toast.LENGTH_SHORT).show();
+                }
+            }, currency);
+        } else {
+            progressBar.setVisibility(View.GONE);
+            mainRv.setVisibility(View.GONE);
+            emptyTv.setVisibility(View.VISIBLE);
+        }
+    }
+}
